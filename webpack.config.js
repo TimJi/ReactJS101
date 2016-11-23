70267b364352c71956b0914c0c47c0c3ce18fae5
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-23 11:29:56 +0800

    sudo npm install -g create-react-app, create-react-app my-app, cd my-app, npm start

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        //query: { presets: ['es2015', 'react'], },
       },
     ],
   },
