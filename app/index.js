acdb4548eda990d4bfcc321b5d4096576f1e5170
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-24 13:09:31 +0800

    Ch3-2 Functional Component

--- app/index.js
+++ app/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//  注意元件開頭第一個字母都要大寫
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, World! by Tim2.0</h1>
-      </div>
-    );
-  }
-}
+// 使用 arrow function 來設計 Functional Component 讓 UI 設計更單純（f(D) => UI），減少副作用（side effect）
+const App = () => (
+  <div>
+    <h1>Hello, World! by Tim2.0</h1>
+  </div>
+);
+
 // 將 <App /> 元件插入 id 為 app 的 DOM 元素中
 ReactDOM.render(<App />, document.getElementById('app'));
