854dc911197bf8e6e7327d7f62c4f3aff88e6fd6
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-24 15:30:04 +0800

    Ch3-3 Component PropType

--- app/index.js
+++ app/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// 使用 arrow function 來設計 Functional Component 讓 UI 設計更單純（f(D) => UI），減少副作用（side effect）
-const App = () => (
-  <div>
-    <h1>Hello, World! by Tim2.0</h1>
-  </div>
-);
+//  注意元件開頭第一個字母都要大寫
+class MyComponent extends React.Component {
+    // render 是 Class based 元件唯一必須的方法（method）
+    render() {
+        return (
+            <div>
+              <h1>Hello, World! by {this.props.name}</h1>
+            </div>
+        );
+    }
+}
 
-// 將 <App /> 元件插入 id 為 app 的 DOM 元素中
-ReactDOM.render(<App />, document.getElementById('app'));
+// PropTypes 驗證，若傳入的 props type 不符合將會顯示錯誤
+MyComponent.propTypes = {
+  name: React.PropTypes.string,
+}
+
+// Prop 預設值，若對應 props 沒傳入值將會使用 default 值
+MyComponent.defaultProps = {
+ name: 'Tim2.0',
+}
+
+// 將 <MyComponent /> 元件插入 id 為 app 的 DOM 元素中
+ReactDOM.render(<MyComponent name = 'Tim2.1'/>, document.getElementById('app'));
