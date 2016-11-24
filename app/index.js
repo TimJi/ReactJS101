7b6ad9431d3727743e2e8dc4f71acf9be385b0bf
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-24 11:10:48 +0800

    CH3-1

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//  注意元件開頭第一個字母都要大寫
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,5 +16,5 @@ class App extends React.Component {
     );
   }
 }
-
+// 將 <App /> 元件插入 id 為 app 的 DOM 元素中
 ReactDOM.render(<App />, document.getElementById('app'));
