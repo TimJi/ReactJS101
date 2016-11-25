1b9bcd34333abcc46532803139cffff9a34162e1
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-25 09:49:08 +0800

    Ch3 with JavaScript

--- app/index.js
+++ app/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//  注意元件開頭第一個字母都要大寫
-class MyComponent extends React.Component {
-    // render 是 Class based 元件唯一必須的方法（method）
-    render() {
-        return (
-            <div>
-              <h1>Hello, World! by {this.props.name}</h1>
-            </div>
-        );
-    }
-}
+// const 為常數
+const lists = ['JavaScript', 'Java', 'Node', 'Python'];
 
-// PropTypes 驗證，若傳入的 props type 不符合將會顯示錯誤
-MyComponent.propTypes = {
-  name: React.PropTypes.string,
-}
-
-// Prop 預設值，若對應 props 沒傳入值將會使用 default 值
-MyComponent.defaultProps = {
- name: 'Tim2.0',
+class HelloMessage extends React.Component {
+  render() {
+    return (
+    <ul>
+      {lists.map((result, index) => {
+        return (<li key={index}>{result}</li>);
+      })}
+    </ul>);
+  }
 }
 
 // 將 <MyComponent /> 元件插入 id 為 app 的 DOM 元素中
-ReactDOM.render(<MyComponent name = 'Tim2.1'/>, document.getElementById('app'));
+ReactDOM.render(<HelloMessage />, document.getElementById('app'));
