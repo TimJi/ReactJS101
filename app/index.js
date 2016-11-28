dc805a9b81bf262bd6463bbce037c8f193630549
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-28 14:35:12 +0800

    Ch4-4 Refs and form

--- app/index.js
+++ app/index.js
@@ -1,49 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// TodoApp 元件中包含了顯示 Todo 的 TodoList 元件，Todo 的內容透過 props 傳入 TodoList 中。由於 TodoList 僅單純 Render UI 不涉及內部 state 操作是 stateless component，所以使用 Functional Component 寫法。需要特別注意的是這邊我們用 map function 來迭代 Todos，需要留意的是每個迭代的元素必須要有 unique key 不然會發生錯誤（可以用自定義 id，或是使用 map function 的第二個參數 index）
-const TodoList = (props) => (
-    <ul>
-        {
-            props.items.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))
-        }
-    </ul>
-)
-
-// 整個 App 的主要元件，這邊比較重要的是事件處理的部份，內部有
-class TodoApp extends React.Component {
+class MarkdownEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.onChange = this.onChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.rawMarkup = this.rawMarkup.bind(this);
         this.state = {
-            items: [],
-            text: '',
+            value: 'Type some *markdown* here!',
         }
     }
-    onChange(e) {
-        this.setState({text: e.target.value});
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        const nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
-        const nextText = '';
-        this.setState({items: nextItems, text: nextText});
+    handleChange() {
+        this.setState({value: this.refs.textarea.value});
+        }
+        // 將使用者輸入的 Markdown 語法 parse 成 HTML 放入 DOM 中，React 通常使用 virtual DOM 作為和 DOM 溝通的中介，不建議直接操作 DOM。故使用時的屬性為 dangerouslySetInnerHTML
+        rawMarkup() {
+        const md = new Remarkable();
+        return { __html: md.render(this.state.value) };
     }
     render() {
         return (
-          <div>
-            <h3>TODO</h3>
-            <TodoList items={this.state.items} />
-            <form onSubmit={this.handleSubmit}>
-              <input onChange={this.onChange} value={this.state.text} />
-              <button>{'Add #' + (this.state.items.length + 1)}</button>
-            </form>
+          <div className="MarkdownEditor">
+            <h3>Input</h3>
+            <textarea
+              onChange={this.handleChange}
+              ref="textarea"
+              defaultValue={this.state.value} />
+            <h3>Output</h3>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={this.rawMarkup()}
+            />
           </div>
         );
     }
 }
 
-ReactDOM.render(<TodoApp />, document.getElementById('app'));
+ReactDOM.render(<MarkdownEditor />, document.getElementById('app'));
