fe6f62e42adc8f2beaa5a4b81ddc62f5a1434dbe
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-28 11:15:10 +0800

    Ch4-2 State

--- app/index.js
+++ app/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
-    // 若是需要綁定 this.方法或是需要在 constructor 使用 props，定義 state，就需要 constructor。若是在其他方法（如 render）使用 this.props 則不用一定要定義 constructor
+class Timer extends React.Component {
     constructor(props) {
-        // 對於 OOP 物件導向程式設計熟悉的讀者應該對於 constructor 建構子的使用不陌生，事實上它是 ES6 的語法糖，骨子裡還是 prototype based 物件導向程式語言。透過 extends 可以繼承 React.Component 父類別。super 方法可以呼叫繼承父類別的建構子
         super(props);
-        this.state = {}
+        // 與 ES5 React.createClass({}) 不同的是 component 內自定義的方法需要自行綁定 this context，或是使用 arrow function
+        this.tick = this.tick.bind(this);
+        // 初始 state，等於 ES5 中的 getInitialState
+        this.state = {
+            secondsElapsed: 0,
+        }
     }
-    // render 是唯一必須的方法，但如果是單純 render UI 建議使用 Functional Component 寫法，效能較佳且較簡潔
+    // 累加器方法，每一秒被呼叫後就會使用 setState() 更新內部 state，讓 Component 重新 render
+    tick() {
+        this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+    }
+    // componentDidMount 為 component 生命週期中階段 component 已插入節點的階段，通常一些非同步操作都會放置在這個階段。這便是每1秒鐘會去呼叫 tick 方法
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000);
+    }
+    // componentWillUnmount 為 component 生命週期中 component 即將移出插入的節點的階段。這邊移除了 setInterval 效力
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    // render 為 class Component 中唯一需要定義的方法，其回傳 component 欲顯示的內容
     render() {
         return (
-            <div>Hello {this.props.name}</div>
-        )
+          <div><h1>Seconds Elapsed: {this.state.secondsElapsed}</h1></div>
+        );
     }
 }
 
-// PropTypes 驗證，若傳入的 props type 不是 string 將會顯示錯誤
-HelloMessage.propTypes = {
-  name: React.PropTypes.string,
-}
-
-// Prop 預設值，若對應 props 沒傳入值將會使用 default 值 Zuck
-HelloMessage.defaultProps = {
- name: 'Zuck',
-}
-
-ReactDOM.render(<HelloMessage name="Mark" />, document.getElementById('app'));
+ReactDOM.render(<Timer />, document.getElementById('app'));
