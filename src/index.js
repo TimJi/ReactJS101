c194d9b57b93a9723e92fda40d06c408a45c6830
Tim2.0 <B10023055@yuntech.edu.tw>
2016-11-30 14:36:36 +0800

    Ch7-1 Flux

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import App from './components/App';
-import Home from './components/Home';
-import Repos from './components/Repos';
-import About from './components/About';
-import User from './components/User';
-import Contacts from './components/Contacts';
-/**/
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="/repos/:name" component={Repos} />
-      <Route path="/about" component={About} />
-      <Route path="/user" component={User} />
-      <Route path="/contacts" component={Contacts} />
-    </Route>
-  </Router>,
-  document.getElementById('app'));
-//*/
-  /* 另外一種寫法： 
-      const routes = (
-          <Route path="/" component={App}>
-            <IndexRoute component={Home} />
-            <Route path="/repos/:name" component={Repos} />
-            <Route path="/about" component={About} />
-            <Route path="/user" component={User} />
-            <Route path="/contacts" component={Contacts} />
-          </Route>
-      );
+import TodoHeader from './components/TodoHeader';
+import TodoList from './components/TodoList';
 
-      ReactDOM.render(
-        <Router routes={routes} history={hashHistory} />,
-        document.getElementById('app'));
-  //*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <TodoHeader />
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
